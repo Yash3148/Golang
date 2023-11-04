8fbc65197d70449705b0a5ab15bcc095941b9ba1
Yash <yashsingh3148@gmail.com>
2023-11-04 22:21:48 +0530

    Golang

---
+++ Printf/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name string = "Jose"
+	var i int = 78
+	fmt.Printf("Hey, %v! You have scored %d/100 in physics", name, i)
+}
+
+// Hey, Jose! You have scored 78/100 in physics
